71475b89b8e3f5ea793a4531db410371b70f901c
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-10-18 09:49:02 +0200

    update app & index

--- holidaze/src/App.js
+++ holidaze/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from "./Components/Layout";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Profile from "./Pages/Profile";
+import VenueDetails from "./Components/VenueDetails";
+import RouteNotFound from "./Components/Layout/RouteNotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />} >
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="Profile" element={<Profile />} />
+        <Route path="venue/:id" element={<VenueDetails />} />
+        <Route path="*" element={<RouteNotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
