b91b456b414abaf2777346e2852a52c1b31266e6
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-10-18 09:50:17 +0200

    Create Constants

---
+++ holidaze/src/Auth/constants/index.jsx
@@ -0,0 +1,4 @@
+export const API_HOST_URL = "https://api.noroff.dev";
+export const API_BASE_URL = "/api/v1";
+export const API_HOLIDAZE_BASE = "/holidaze";
+export const API_HOLIDAZE_URL = `${API_HOST_URL}${API_BASE_URL}${API_HOLIDAZE_BASE}`;
