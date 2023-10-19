a53e6657a87c32b9090bb28df74cb885157106d3
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-10-19 14:38:43 +0200

    Update LocalStorage.js

--- holidaze/src/Auth/utils/LocalStorage.js
+++ holidaze/src/Auth/utils/LocalStorage.js
@@ -9,3 +9,27 @@ export const getToken = () => {
 export const removeToken = () => {
     localStorage.removeItem('token');
 };
+
+
+
+function isJsonString(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
+export function load(key) {
+    try {
+        const value = localStorage.getItem(key);
+        if (isJsonString(value)) {
+            return JSON.parse(value);
+        }
+        return value;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
