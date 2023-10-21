2b9b71096ed8b1812b55ce207021ae6f85f23401
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-10-21 11:46:48 +0200

    User Profile ( Not working) - issue -> AuthContext)

---
+++ holidaze/src/Pages/Profile/UserProfile/UserProfile.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useAuth } from "../../../Auth/context/AuthContext";
+import styles from "./UserProfile.module.css";
+import ToggleVenueManager from "../ToggleVenueManager/Toggle";
+
+function UserProfile() {
+  const { user } = useAuth();
+
+  return (
+    <div className={styles.profile}>
+      {user ? (
+        <div className={styles.profileDetails}>
+          <h2>Welcome {user.name}</h2>
+          <p className={styles.profileItem}>Name: {user.name}</p>
+          <p className={styles.profileItem}>Email: {user.email}</p>
+          <p className={styles.profileItem}>
+            Role: {user.venueManager ? "Venue Manager" : "Regular User"}
+            <ToggleVenueManager />
+          </p>
+          <img className={styles.avatar} src={user.avatar} alt="User Avatar" />
+          {/* Add more user profile details here */}
+        </div>
+      ) : (
+        <p className={styles.loading}>Loading user profile...</p>
+      )}
+    </div>
+  );
+}
+
+export default UserProfile;
--- holidaze/src/Pages/Profile/index.jsx
+++ holidaze/src/Pages/Profile/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import UserProfile from "./UserProfile";
+import UserProfile from "./UserProfile/UserProfile";
+import UpdateForm from "./Avatar/UpdateForm";
 
 function Profile() {
   return (
     <div>
       <UserProfile />
+      <UpdateForm />
     </div>
   );
 }
