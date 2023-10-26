e2aa96ecac0efe7355debfab108ef543de5ed16c
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-10-26 20:55:44 +0200

    Modal

---
+++ holidaze/src/Components/Modal/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./Modal.module.scss";
+
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className={styles.customModal}>
+      <div className={styles.modalContent}>
+        <button className={styles.closeButton} onClick={onClose}>
+          X
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Modal;
--- holidaze/src/Pages/Profile/index.jsx
+++ holidaze/src/Pages/Profile/index.jsx
@@ -4,12 +4,26 @@ import UpdateForm from "./Avatar/UpdateForm";
 import ToggleVenueManager from "./ToggleVenueManager/Toggle";
 import CreateVenue from "../../Components/CreateVenue";
 import ProfileVenues from "./ProfileVenues";
+import { useState } from "react";
+import Modal from "../../Components/Modal";
 
 function Profile() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   return (
     <div>
       <UserProfile />
-      <UpdateForm />
+      <button onClick={openModal}>Open Update Form</button>
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <UpdateForm />
+      </Modal>
       <ToggleVenueManager />
       <CreateVenue />
       <ProfileVenues />
