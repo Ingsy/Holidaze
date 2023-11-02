2c31ebe07b933c00180f3a695b91ddf36fb1b5f3
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-11-02 12:12:14 +0100

    Create YourVenueGrid.jsx

---
+++ holidaze/src/Components/VenueGrid/YourVenueGrid.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./VenueGrid.module.css";
+import StarRating from "../StarRating";
+
+const placeholderImageUrl = "https://picsum.photos/200/300"; // The original image size
+
+function VenueGrid({ venues, loading }) {
+  return (
+    <div className={styles.container}>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="row">
+          {venues.map((venue) => (
+            <div key={venue.id} className="row">
+              <div className={styles.YourVenueCard} tabIndex="0">
+                <h2 className={`${styles.venueName} text-center`}>
+                  {venue.name.length > 30
+                    ? `${venue.name.substring(0, 30)}...`
+                    : venue.name}
+                </h2>
+                {venue.media.length > 0 ? (
+                  <img
+                    src={venue.media[0]} // You can add width and height attributes here to make the image smaller
+                    alt={`Venue Media`}
+                    className={styles.YourMedia}
+                  />
+                ) : (
+                  <img
+                    src={placeholderImageUrl} // Use the same placeholder image
+                    alt={`No Media Available`}
+                    className={styles.YourMedia}
+                    width="150"
+                    height="200"
+                  />
+                )}
+                <div className={`d-flex ${styles.venueInfo}`}>
+                  <p className={`flex-grow-1 ${styles.YourVenuePrice}`}>
+                    ${venue.price} night
+                  </p>
+                  <StarRating rating={venue.rating} />
+                </div>
+                {venue.location.city && venue.location.country ? (
+                  <p className={styles.YourVenueLocation}>
+                    {venue.location.country}, {venue.location.city}
+                  </p>
+                ) : (
+                  <p className={styles.YourVenueLocation}>Exciting Location</p>
+                )}
+
+                <div className="mt-auto text-center">
+                  <Link
+                    to={`/venue/${venue.id}`}
+                    className={styles.Link}
+                    tabIndex="0"
+                  >
+                    Checkout Venue
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default VenueGrid;
