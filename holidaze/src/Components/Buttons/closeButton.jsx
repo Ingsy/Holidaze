50963ee252a2700f873a4209e42b37c6f04411a0
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-11-06 14:56:17 +0100

    ...

--- holidaze/src/Components/Buttons/closeButton.jsx
+++ holidaze/src/Components/Buttons/closeButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
-import styles from "./";
+import styles from "./closeButton.module.scss";
 
 const CloseButton = ({ onClick }) => {
   return (
