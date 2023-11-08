aee65605eb3f0c6f694d9b02573d3b1abb04fe4b
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-11-08 19:58:27 +0100

    Update BookingCreate.jsx

--- holidaze/src/Components/Booking/BookingCreate.jsx
+++ holidaze/src/Components/Booking/BookingCreate.jsx
@@ -2,24 +2,23 @@ import React from "react";
 import { BookingForm } from "./bookingForm";
 import { createBooking } from "./bookingsAPI";
 import { useNavigate } from "react-router-dom";
-
+import { useHolidaizApi } from "../../Auth/constants";
 import styles from "./Booking.module.scss";
 
 export const BookingCreate = ({ onCreateError, venueId, maxGuests }) => {
   console.log("Venue ID in BookingCreate:", venueId);
   const navigate = useNavigate();
+  const { bookings } = useHolidaizApi();
 
   const handleFormChange = (formData) => {
     console.log("Form data changed:", formData);
   };
 
-  const onSave = (booking, formData) => {
-    console.log("Booking object before setting venueId:", booking);
-    booking.venueId = formData.venueId;
-    console.log("Booking object after setting venueId:", booking);
-    createBooking(booking)
+  const onSave = (booking) => {
+    bookings
+      .create(booking)
       .then((data) => {
-        navigate(`/booking/${data.id}`);
+        window.location.reload();
       })
       .catch((error) => {
         console.log("Create booking error", error);
