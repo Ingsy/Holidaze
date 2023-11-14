7527447955f45be8eb181118f42fa59db2b72c91
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-11-14 13:41:34 +0100

    Update VenueCreate.jsx

--- holidaze/src/Components/Venue/VenueCreate.jsx
+++ holidaze/src/Components/Venue/VenueCreate.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { VenueForm } from "./VenueForm";
-//import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useHolidaizApi } from "../../Auth/constants";
 import styles from "../../Styles/Venue.module.css";
 
 export const VenueCreate = ({ onCreateError }) => {
-  //const navigate = useNavigate();
+  const navigate = useNavigate();
   const { venues } = useHolidaizApi();
 
   const onSave = (venue) => {
     venues
       .create(venue)
       .then((data) => {
-        window.location.reload();
-      }) //navigate(`/venue/${data.id}`))
+        navigate(`/venue/${data.id}`);
+      })
       .catch((error) => {
         console.log("Create venue error", error);
         if (onCreateError) onCreateError(error);
