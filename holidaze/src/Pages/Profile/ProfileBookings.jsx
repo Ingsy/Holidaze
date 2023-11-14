1f6bb5da7876a0b8bbd1db57f06dda98ceb816d8
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-11-14 13:44:11 +0100

    Update ProfileBookings.jsx

--- holidaze/src/Pages/Profile/ProfileBookings.jsx
+++ holidaze/src/Pages/Profile/ProfileBookings.jsx
@@ -1,21 +1,19 @@
 import React, { useState, useEffect } from "react";
-//import YourVenueGrid from "../../Components/VenueGrid/YourVenueGrid";
+import YourVenueGrid from "../../Components/VenueGrid/YourVenueGrid";
 import { useHolidaizApi } from "../../Auth/constants";
-import BookingList from "../../Components/Booking/BookingList";
+//import BookingList from "../../Components/Booking/BookingList";
 
 export const ProfileBookings = () => {
-  const [bookings, setBookings] = useState([]);
+  //const [bookings, setBookings] = useState([]);
+  const [venues, setVenues] = useState([]);
   const [loading, setLoading] = useState(true);
-  const { bookings: holidaizBookings } = useHolidaizApi(); // Using 'holidaizBookings' to avoid redeclaration
+  const { bookings: holidaizBookings } = useHolidaizApi();
 
   useEffect(() => {
     async function fetchUserBookings() {
       try {
-        console.log("Fetching user bookings...");
-        const userBookings = await holidaizBookings.get(); // Use 'GET' to retrieve user bookings
-        console.log("User bookings retrieved:", userBookings);
-
-        setBookings(userBookings);
+        const userBookings = await holidaizBookings.get({ _bookings: true });
+        setVenues(userBookings);
         setLoading(false);
       } catch (error) {
         console.error("Error fetching user bookings:", error);
@@ -26,11 +24,9 @@ export const ProfileBookings = () => {
     fetchUserBookings();
   }, [holidaizBookings]);
 
-  console.log("Bookings in state:", bookings);
-
   return (
     <div>
-      <BookingList bookings={bookings} loading={loading} />
+      <YourVenueGrid venues={venues} loading={loading} />
     </div>
   );
 };
