9fce5c9cf88de7e965ca1166e2f5bfcdb7dcdac1
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-11-14 13:41:24 +0100

    Update VenueList.jsx

--- holidaze/src/Components/Venue/VenueList.jsx
+++ holidaze/src/Components/Venue/VenueList.jsx
@@ -1,28 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import VenueGrid from "../VenueGrid";
-function VenueList() {
-  const [venues, setVenues] = useState([]);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await fetch(
-          "https://api.noroff.dev/api/v1/holidaze/venues"
-        );
-        const data = await response.json();
-        setVenues(data);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        setLoading(false);
-      }
-    }
+function VenueList({ venues }) {
+  console.log("Venues in VenueList:", venues);
 
-    fetchData();
-  }, []);
-
-  return <VenueGrid venues={venues} loading={loading} />;
+  return <VenueGrid venues={venues} />;
 }
 
 export default VenueList;
