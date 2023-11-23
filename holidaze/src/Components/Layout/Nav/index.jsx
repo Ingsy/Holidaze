e7b1bfc97b8b71e403323d4c3839459145fcc6d0
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-11-23 09:51:39 +0100

    Update index.jsx

--- holidaze/src/Components/Layout/Nav/index.jsx
+++ holidaze/src/Components/Layout/Nav/index.jsx
@@ -3,12 +3,14 @@ import { Link, useLocation } from "react-router-dom";
 import LoginLogoutVenueManager from "../../LoginLogoutNav";
 import Search from "../../Search/Search";
 import { useHolidaizApi } from "../../../Auth/constants";
+import { useAuth } from "../../../Auth/context/AuthContext";
 import styles from "../../../Styles/Nav.module.scss";
 
 function Navbar({ onSearch }) {
   const { venues } = useHolidaizApi();
-
+  const { user } = useAuth();
   const location = useLocation();
+  const isLoggedIn = !!user.token;
 
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -34,28 +36,32 @@ function Navbar({ onSearch }) {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav d-flex align-items-lg-center justify-content-lg-center me-auto mb-2 mb-lg-0 mx-auto">
-            <li className="nav-item m-2">
-              <Link
-                to="/"
-                className={`${styles.navLink} ${
-                  location.pathname === "/" ? styles.active : ""
-                }`}
-                aria-current="page"
-              >
-                Home
-              </Link>
-            </li>
-            <li className="nav-item m-2 me-5">
-              <Link
-                to="/Profile"
-                className={`${styles.navLink} ${
-                  location.pathname === "/Profile" ? styles.active : ""
-                }`}
-                aria-current="page"
-              >
-                Profile
-              </Link>
-            </li>
+            {isLoggedIn && (
+              <>
+                <li className="nav-item m-2">
+                  <Link
+                    to="/"
+                    className={`${styles.navLink} ${
+                      location.pathname === "/" ? styles.active : ""
+                    }`}
+                    aria-current="page"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item m-2 me-5">
+                  <Link
+                    to="/Profile"
+                    className={`${styles.navLink} ${
+                      location.pathname === "/Profile" ? styles.active : ""
+                    }`}
+                    aria-current="page"
+                  >
+                    Profile
+                  </Link>
+                </li>
+              </>
+            )}
             <li className="nav-item m-2">
               <LoginLogoutVenueManager />
             </li>
